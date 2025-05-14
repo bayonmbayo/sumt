9c9bdbed796ed037e5a12e1cd6663672a034a136
Bayon Mbayo <bayon2007@gmail.com>
2025-05-15 01:28:45 +0200

    Manage Users

--- src/constants/worker.constants.js
+++ src/constants/worker.constants.js
@@ -1,5 +1,5 @@
 export const workerConstants = {
     // WORKER: 'https://service.onelike.app/sumt/',
     // WORKER: 'http://localhost:8080/sumt/',
-    WORKER: 'http://144.91.120.172/sumt/'
+    WORKER: 'https://144.91.120.172/sumt/'
 };
