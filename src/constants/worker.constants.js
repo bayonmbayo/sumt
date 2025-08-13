20ac00eee170f2a9d434913c1fbeba22bfb8f0ed
Bayon Mbayo <bayon2007@gmail.com>
2025-08-14 00:29:52 +0200

    ADD: Synchronization Simulation

--- src/constants/worker.constants.js
+++ src/constants/worker.constants.js
@@ -1,6 +1,6 @@
 export const workerConstants = {
     // WORKER: 'https://service.onelike.app/sumt/',
     // WORKER: 'http://localhost:8080/sumt/',
-    // WORKER: 'http://144.91.120.172/sumt/'
-    WORKER: 'https://api.onelike.app/sumt/',
+    WORKER: 'http://144.91.120.172/sumt/'
+    // WORKER: 'https://api.onelike.app/sumt/',
 };
